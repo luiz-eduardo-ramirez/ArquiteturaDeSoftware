896a6d7010d6197ae1885d8f8853862d40c67754
caiohportella <caiohportella@gmail.com>
2022-05-18 16:46:52 -0300

    First mai menu implementation complete

--- UnityProject/ASLeitner/Assets/#Game/Scripts/MainMenu.cs
+++ UnityProject/ASLeitner/Assets/#Game/Scripts/MainMenu.cs
@@ -1,29 +1,44 @@
+using ASLeitner.Managers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+namespace ASLeitner.Menu
 {
-    public void SortFlashcard()
+    public class MainMenu : MonoBehaviour
     {
-        SceneManager.LoadScene("Sorting");
-        Debug.Log("Sorteando flashcard!");
-    }
+        int CardQuantity { get => PlayerDataManager.Instance.PlayerDeck.DeckSize; }
+        public void SortFlashcard()
+        {
+            if (CardQuantity >= 10)
+            {
+                SceneManager.LoadScene("Sorting");
+                Debug.Log("Sorteando flashcard!");
+            }
+        }
 
-    public void InsertRemoveFlashcard()
-    {
-        SceneManager.LoadScene("InsertRemove");
-        Debug.Log("Inserindo/removendo flashcard!");
-    }
+        public void InsertRemoveFlashcard()
+        {
+            SceneManager.LoadScene("InsertRemove");
+            Debug.Log("Inserindo/removendo flashcard!"); 
+        }
 
-    public void ShareFlashcard()
-    {
-        SceneManager.LoadScene("Share");
-        Debug.Log("Compartilhando flashcard!");
-    }
+        public void ShareFlashcard()
+        {
+            if (CardQuantity == 0)
+            {
+                Debug.Log("Numero minimo de flashcards = 1");
+            }
+            else
+            {
+                SceneManager.LoadScene("Share");
+                Debug.Log("Compartilhando flashcard!");
+            }
+        }
 
-    public void QuitApplication()
-    {
-        Application.Quit();
-        Debug.Log("Saindo da aplicação...");
+        public void QuitApplication()
+        {
+            Application.Quit();
+            Debug.Log("Saindo da aplicação...");
+        }
     }
 }
