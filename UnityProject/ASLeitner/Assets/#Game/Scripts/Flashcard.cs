314f01cea13204b6a8e0476b2d8b28eed20bd595
Arthur Pedroso <arthurnaves@hotmail.com.br>
2022-05-31 19:18:19 -0300

    Sistema de indicacao a qual caixa o flashcard pertence

--- UnityProject/ASLeitner/Assets/#Game/Scripts/Flashcard.cs
+++ UnityProject/ASLeitner/Assets/#Game/Scripts/Flashcard.cs
@@ -1,6 +1,6 @@
 using ASLeitner.DataStructs;
 using ASLeitner.Managers;
-using Base.Extensions.Utilites;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -14,6 +14,16 @@ namespace ASLeitner
         private TextMeshPro m_termo;
         [SerializeField]
         private TextMeshPro m_definicao;
+        [SerializeField]
+        private Color m_ignoranceColor;
+        [SerializeField]
+        private Color m_superficialColor;
+        [SerializeField]
+        private Color m_acquiredColor;
+        [SerializeField]
+        private Renderer m_rendererBack;
+        [SerializeField]
+        private Renderer m_rendererFront;
         public bool ShowingTerm { get; set; }
         public bool LookForward { get; set; }
 
@@ -35,6 +45,24 @@ namespace ASLeitner
             m_termo.text = _flashcardData.CardFront;
             m_definicao.text = _flashcardData.CardBack;
             FlashcardData = _flashcardData;
+
+            switch (_flashcardData.LearningStage)
+            {
+                case LearningStages.Ignorant:
+                    m_rendererBack.material.SetColor("_EmissionColor", m_ignoranceColor);
+                    m_rendererFront.material.SetColor("_EmissionColor", m_ignoranceColor);
+                    break;
+                case LearningStages.Superficial:
+                    m_rendererBack.material.SetColor("_EmissionColor", m_superficialColor);
+                    m_rendererFront.material.SetColor("_EmissionColor", m_superficialColor);
+                    break;
+                case LearningStages.Acquired:
+                    m_rendererBack.material.SetColor("_EmissionColor", m_acquiredColor);
+                    m_rendererFront.material.SetColor("_EmissionColor", m_acquiredColor);
+                    break;
+                default:
+                    throw new Exception("Estagio de aprendizado inexistente");
+            }
         }
         public void SetTermVisibility(bool _setValue) 
         {
